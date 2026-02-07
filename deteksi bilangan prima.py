d7afc0d7150a507fffb667a8bba543f5a414c21e
wajibro <wajibro@yahoo.com>
2026-02-07 02:35:17 +0000

    Membetulkan program periksa angka prima

--- deteksi bilangan prima.py
+++ deteksi bilangan prima.py
@@ -2,17 +2,37 @@ data = list(range(1, 101))
 
 def cek_prima(n):
     match n:
-        case x if x < 2:
-            return
         case 2:
-            return n
-        case x if x % 2 == 0:
-            return
-    
-    for i in range(3, int(n ** .5) + 1):
-        if n % i == 0:
-            return
-    return n
+            return True
+        case x if x < 2:
+            return False
+        case _:
+            for i in range(2, n):
+                if n % i == 0:
+                    return False
+            return True
 
-angka_prima = [x for x in data if cek_prima(x)]
-print(angka_prima)
+status = 0
+print("""
+==============================
+1. Cek Prima berdasarkan input
+2. Cek Prima 1 - 100
+==============================
+""")
+status = input('Masukkan opsi anda: ')
+status = int(status)
+match status:
+    case 1:
+        input_prima = input('Masukkan angka yang ingin diperiksa: ')
+        input_prima = int(input_prima)
+        print(f'Angka {input_prima} adalah', "benar angka prima" if cek_prima(input_prima) else "bukan angka prima")
+    case 2:
+        for i in data:
+            if cek_prima(i):
+                print(i," ", end='')
+    case _:
+        print("""
+1. Cek Prima berdasarkan input
+2. Cek Prima 1 - 100
+""")
+        status = input('Masukkan opsi anda: ')
